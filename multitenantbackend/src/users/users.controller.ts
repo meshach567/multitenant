897528e424810c940e721a30bcf804860e1e3901
Meshach Arinze <arinzemeshach123@gmail.com>
2025-12-30 19:10:40 +0100

    feat: get the id of a users

---
+++ multitenantbackend/src/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
+import { UsersService } from './users.service';
+import {
+  CurrentUser,
+  CurrentUserPayload,
+} from '../common/decorators/current-user.decorator';
+
+@Controller('users')
+@UseGuards(JwtAuthGuard)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('me')
+  getProfile(@CurrentUser() user: CurrentUserPayload) {
+    return this.usersService.findById(user.userId);
+  }
+}
