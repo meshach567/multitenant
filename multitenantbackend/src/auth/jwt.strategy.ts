f9a3407849947dfcd0dd5ea242009c55f5cd837d
Meshach Arinze <arinzemeshach123@gmail.com>
2025-12-30 11:07:08 +0100

    feat(jwt): implemenAuth Header Bear Token

---
+++ multitenantbackend/src/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from './types/jwt-payload.type';
+import { AuthenticatedUser } from './types/authenticated-user.type';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(config: ConfigService) {
+    const options: StrategyOptions = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.getOrThrow<string>('JWT_ACCESS_SECRET'),
+    };
+    super(options);
+  }
+
+  validate(payload: JwtPayload): AuthenticatedUser {
+    return {
+      userId: payload.sub,
+      role: payload.role,
+      businessId: payload.businessId,
+    };
+  }
+}
