1962fc3cc86818712f56f843817792002b2d43d4
Meshach Arinze <arinzemeshach123@gmail.com>
2025-12-30 10:49:37 +0100

    fix(prisModule) add Prisma module to app modules

--- multitenantbackend/src/app.module.ts
+++ multitenantbackend/src/app.module.ts
@@ -2,12 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { BusinessModule } from './business/business.module';
+import { PrismaModule } from './common/prisma/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    BusinessModule,
   ],
   controllers: [AppController],
   providers: [AppService],
