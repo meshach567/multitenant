364e431f1e4955cc4244ba576826d316b9ef6a8f
Meshach Arinze <arinzemeshach123@gmail.com>
2025-12-30 19:16:18 +0100

    feat: find a user by email

---
+++ multitenantbackend/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../common/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(userId: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        role: true,
+        businessId: true,
+        createdAt: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
+
+  async findByEmail(email: string): Promise<unknown> {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
