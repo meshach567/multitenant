ce106a4f9f5c2b6f0b274c815103c637a67edc28
Meshach Arinze <arinzemeshach123@gmail.com>
2025-12-30 10:50:50 +0100

    fix(promise): add a catch block bootstrap

--- multitenantbackend/src/main.ts
+++ multitenantbackend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.PORT ?? 3000);
+  } catch (error) {
+    console.error('Bootstrap error:', error);
+    process.exit(1);
+  }
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Unhandled bootstrap error:', err);
+});
