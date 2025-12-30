11199d4543a28addd22f4fc981a4b0e80453b0b5
Meshach Arinze <arinzemeshach123@gmail.com>
2025-12-30 10:44:55 +0100

    chore(prisma): debug to check if error exists

---
+++ multitenantbackend/prisma.config.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const config_1 = require("prisma/config");
+exports.default = (0, config_1.defineConfig)({
+    schema: 'src/prisma/schema.prisma',
+    migrations: {
+        path: 'src/prisma/migrations',
+    },
+    datasource: {
+        url: process.env['DATABASE_URL'],
+    },
+});
+//# sourceMappingURL=prisma.config.js.map
