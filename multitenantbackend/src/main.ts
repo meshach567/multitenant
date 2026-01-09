575fa3d8bf5f7d0688f9a43f79526adb61a26a76
Meshach Arinze <arinzemeshach123@gmail.com>
2026-01-09 09:49:31 +0100

    refactor: add cookie parser and enable cors origin to interact with frontend

--- multitenantbackend/src/main.ts
+++ multitenantbackend/src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe());
-    await app.listen(process.env.PORT ?? 3000);
+    app.use(cookieParser());
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+      }),
+    );
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    });
+    await app.listen(process.env.PORT ?? 3001);
   } catch (error) {
     console.error('Bootstrap error:', error);
     process.exit(1);
