195eb3b0bdbe7a015226391c441f0bcd062f5e9a
Meshach Arinze <arinzemeshach123@gmail.com>
2026-01-09 10:04:53 +0100

    refactor: replace the Bearer token with cookies

--- multitenantbackend/src/auth/jwt.strategy.ts
+++ multitenantbackend/src/auth/jwt.strategy.ts
@@ -3,13 +3,18 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy, StrategyOptions } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
 import { JwtPayload } from './types/jwt-payload.type';
+//import { AuthenticatedUser } from './types/authenticated-user.type';
+//import { CurrentUserPayload } from '../common/decorators/current-user.decorator';
+import { Role } from './roles.enum';
 import { AuthenticatedUser } from './types/authenticated-user.type';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService) {
     const options: StrategyOptions = {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (req) => req?.cookies?.accessToken,
+      ]),
       secretOrKey: config.getOrThrow<string>('JWT_ACCESS_SECRET'),
     };
     super(options);
@@ -18,7 +23,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   validate(payload: JwtPayload): AuthenticatedUser {
     return {
       userId: payload.sub,
-      role: payload.role,
+      role: payload.role as Role,
       businessId: payload.businessId,
     };
   }
