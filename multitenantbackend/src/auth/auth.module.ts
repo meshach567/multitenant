bda86877017e4cd29f5b551760ecc0771d5c46aa
Meshach Arinze <arinzemeshach123@gmail.com>
2026-01-09 09:52:42 +0100

    feat: add Mail module to auth module

--- multitenantbackend/src/auth/auth.module.ts
+++ multitenantbackend/src/auth/auth.module.ts
@@ -4,9 +4,13 @@ import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { PrismaService } from '../common/prisma/prisma.service';
+// import { MailService } from '../mail/mail.service';
+import { MailModule } from '../mail/mail.module';
 
 @Module({
   imports: [
+    MailModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
@@ -17,7 +21,7 @@ import { JwtStrategy } from './jwt.strategy';
       }),
     }),
   ],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, PrismaService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
