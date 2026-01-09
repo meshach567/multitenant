15ec969fddd62b064d087fc6d36dd6740fd07f64
Meshach Arinze <arinzemeshach123@gmail.com>
2026-01-09 09:48:39 +0100

    refactor: add the Staff and Mail nodule to AppModule

--- multitenantbackend/src/app.module.ts
+++ multitenantbackend/src/app.module.ts
@@ -6,6 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BusinessModule } from './business/business.module';
 import { PrismaModule } from './common/prisma/prisma.module';
+import { StaffController } from './staff/staff.controller';
+import { StaffModule } from './staff/staff.module';
+import { MailController } from './mail/mail.controller';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -14,10 +18,12 @@ import { PrismaModule } from './common/prisma/prisma.module';
     }),
     PrismaModule,
     AuthModule,
+    StaffModule,
     UsersModule,
     BusinessModule,
+    MailModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, StaffController, MailController],
   providers: [AppService],
 })
 export class AppModule {}
