954c308f99a7d1aedbcd76a85c4fee20454863ad
Meshach Arinze <arinzemeshach123@gmail.com>
2026-01-09 09:50:29 +0100

    refactor: add thee route for verify email, forgot password, and others

--- multitenantbackend/src/auth/auth.controller.ts
+++ multitenantbackend/src/auth/auth.controller.ts
@@ -4,34 +4,61 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  UnauthorizedException,
+  Res,
+  // UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { ForgotPasswordDto } from './dto/forgot-password.dto';
+import { ResetPasswordDto } from './dto/reset-password.dto';
+import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly auth: AuthService) {}
 
-  @HttpCode(HttpStatus.OK)
+  // @HttpCode(HttpStatus.OK)
   @Post('register')
   register(@Body() dto: RegisterDto) {
     return this.auth.register(dto.email, dto.password);
   }
 
+  @Post('verify-email')
+  async verifyEmail(@Body() dto: VerifyEmailDto) {
+    return await this.auth.verifyEmail(dto);
+  }
+
+  @Post('resend-verification')
+  async resendVerification(@Body('email') email: string) {
+    return this.auth.resendVerification(email);
+  }
+
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
-    const user = await this.auth.validateUser(body.email, body.password);
+  async login(
+    @Body() dto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { accessToken, user } = await this.auth.loginWithCredentials(dto);
 
-    if (!user) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
-
-    return this.auth.login({
-      id: user.id,
-      role: user.role,
-      businessId: user.businessId ?? undefined,
+    res.cookie('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'lax',
     });
+
+    return { user };
+  }
+
+  @Post('forgot-password')
+  async forgotPassword(@Body() dto: ForgotPasswordDto) {
+    return this.auth.forgotPassword(dto.email);
+  }
+
+  @Post('reset-password')
+  async resetPassword(@Body() dto: ResetPasswordDto) {
+    return this.auth.resetPassword(dto);
   }
 }
